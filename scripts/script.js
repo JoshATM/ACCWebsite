73766df0d39359521be938d8c1b2c7c2bf06e33e
JoshATM <joshua.downes.747@accesstomusic.ac.uk>
2024-02-02 12:18:11 +0000

    update: Layout and Styling

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
-let x = 1;
+function HomePage() {
+    console.log("Home Page");
+    window.location.href = "index.html";
+}
+
+function Projects() {
+    console.log("Projects");
+    window.location.href = "projects.html";
+}
+
+function GeorgeBrennanWebsite() {
+    console.log("George Brennan Website");
+    window.location.href = "https://GeorgeBrennan.uk/";
+}
