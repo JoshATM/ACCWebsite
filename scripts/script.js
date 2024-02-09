89cba36d73907798dbd4e4b65d738a2ed6d1e6c5
JoshATM <joshua.downes.747@accesstomusic.ac.uk>
2024-02-09 11:00:11 +0000

    Updated: Landing Page redirects to CV
    Got rid off Skills section as it was uneeded

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,3 @@
-function HomePage() {
-    window.location.href = "index.html";
-}
-
 function Projects() {
-    window.location.href = "projects.html";
-}
-
-function GeorgeBrennanWebsite() {
-    window.location.href = "https://GeorgeBrennan.uk/";
-}
-
-function JoshuaDownes() {
-    window.location.href = "https://JDownes.uk/";
+    window.location.href = "";
 }
